f4e55aef5add1e9673ae7e4ff163352f9de7ef37
muhammed3iid <muhammed3iid@gmail.com>
2023-12-25 00:46:35 +0200

    register, login pages added

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Flight Booking Website</title>
+</head>
+
+<body>
+    <?php include 'includes/header.html'; ?>
+    <main>
+        <section>
+            <h2>
+                Welcome to your flight booking website
+            </h2>
+            <p>
+                Explore our services and features. Log in or register to get started.
+            </p>
+            <p>
+                <a href="pages/register_page.html">Register</a> or <a href="pages/login_page.html">Log In</a>
+            </p>
+        </section>
+    </main>
+</body>
+</html>
